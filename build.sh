8f9f0db0fe79564f52dfbe9a9c6882d5ac17cb50
xiezongjun <xiezjsc@gmail.com>
2017-01-06 16:22:13 +0800

    update to docker-compose

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t yii2-advanced ./dockerfiles/yii2-advanced
-docker build -t php7 ./dockerfiles/php7
-docker build -t php7-dev ./dockerfiles/php7-dev
--- run_proxy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker rm -f proxy
-docker run --name proxy --restart=always -d -p 443:443 -p 80:80 -v `pwd`/files/certs:/etc/nginx/certs -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
